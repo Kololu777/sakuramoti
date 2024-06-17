e865a07c6775b3c5feb472f5df2bce3f50917e4d
Kololu777 <kosiynon31kos@gmail.com>
2024-06-17 23:55:57 +0900

    add utils code

---
+++ src/sakuramoti/io/io.py
@@ -0,0 +1,11 @@
+import numpy as np
+import torch
+from torch import Tensor
+from PIL import Image
+
+
+def load_image(path_file:str, device:str)->Tensor:
+    img = np.array(Image.open(path_file)).astype(np.uint8)
+    img = torch.from_numpy(img).permute(2, 0, 1).float()
+    img = 2 * (img/ 255.0) - 1.0    
+    return img[None].to(device)

9b6e8f8cb097a032c9a6e2623ab065350d6ab20b
Kololu777 <kosiynon31kos@gmail.com>
2024-08-12 18:32:41 +0900

    Feat: loss and metrics

--- src/sakuramoti/losses/sequence_loss.py
+++ src/sakuramoti/losses/sequence_loss.py
@@ -1,58 +1,94 @@
+from __future__ import annotations
 import torch
 import torch.nn as nn
 from torch import Tensor
-
-def maksed_mean(loss, valid, mean_mode, eps=1e-8):
-    if mean_mode == "mean":
-        return (valid * loss).mean()
-    
-    elif mean_mode == "reduce_masked_mean":
-        prod = loss * valid
-        numer = torch.sum(prod)
-        denom = eps+torch.sum(valid)
-        return numer / denom
+from .utils import maksed_mean
 
 
-
-def sequence_loss(input:list[Tensor], 
-                  target:Tensor, 
-                  valids:Tensor,
-                  gamma:float=0.8, 
-                  mean_mode:str='reduce_masked_mean'):
-    """Criterion that compute sequence loss [1][2] similar to l1 loss.
+def sequence_loss(
+    inputs: list[Tensor],
+    target: Tensor,
+    valid: Tensor | None = None,
+    gamma: float = 0.8,
+    mean_mode: str = "mean",
+):
+    r"""Criterion that compute sequence loss [1][2] similar to l1 loss.
 
     Reference:
         [1] RAFT: https://arxiv.org/pdf/2003.12039
         [2] PIPs: https://arxiv.org/pdf/2204.04153
 
-    Args:
-        input (list[Tensor]): The predicted tensor. Shape of ()
-        target (Tensor): The target tensor. Shape of ()
-        valids (Tensor): 
-        gamma (float, optional): exponentially increasing weights.
-        mean_mode (str, optional):
-            - ``'reduce_masked_mean'``: PIPs Sequence Loss: Calculating the average excluding 0 from the valid values.
-            - ``'mean'``: RAFT Sequence loss: Calculating the average the average from tensor.
+    Parameters:
+        input: The predicted tensor. Shape of (B, T, N, 2).
+        target: The target tensor. Shape of (B, T, N, 2).
+        valid: Shape of (B, N, 2). If `valid = None`, `valid = torch.ones(B, N, 2)`
+        gamma: Exponentially increasing weights.
+        mean_mode:
+            - ``'mean'`` [default]: RAFT Sequence loss: Calculating the average the average from tensor.
+            - ``'reduce_masked_mean'`` PIPs Sequence Loss: Calculating the average excluding 0 from the valid values.
 
     Returns:
-        _type_: _description_
+        one-element tensor of the computed loss.
+
+    Example:
+        >>> T = 8
+        >>> input = torch.rand(32, T, 100, 2) #(B, T, N, 2)
+        >>> inputs = [input for _ in range(3)] # iteration
+        >>> output = sequence_loss(inputs, target)
+        >>> output.backward()
+
     """
-    n = len(input)
-    flow_loss = 0.0    
-    for idx in range(1, n + 1, 1): 
-        weight =  gamma ** (n - idx)
-        loss = torch.mean((input[idx] - target).abs(), dim=3)
-        flow_loss += weight * maksed_mean(loss, valids)
+    n = len(inputs)
+    flow_loss = 0.0
+    for idx in range(n):
+        weight = gamma ** (n - (idx + 1))
+        loss = torch.mean((inputs[idx] - target).abs(), dim=3)
+        flow_loss += weight * maksed_mean(loss, valid, mean_mode)
     return flow_loss
-            
+
+
 class SequenceLoss(nn.Module):
-    def __init__(self, gamma:float=0.8, max_flow:int=400):
+    r"""Criterion that compute Sequence Loss.
+
+    Reference:
+        [1] RAFT: https://arxiv.org/pdf/2003.12039
+        [2] PIPs: https://arxiv.org/pdf/2204.04153
+
+    Parameters:
+        gamma: Exponentially increasing weights.
+        mean_mode:
+            - ``'mean'`` [default]: RAFT Sequence loss: Calculating the average the average from tensor.
+            - ``'reduce_masked_mean'`` PIPs Sequence Loss: Calculating the average excluding 0 from the valid values.
+
+    Shape:
+       - input: :math:`(B, T, N, 2)` where T = number of frame, N = number of keypoints
+       - target: :math:`(B, T, N, 2)`.
+       - valid: :math:`(B, T, N)` where type of vaild is torch.bool.
+
+    Example:
+        >>> T = 8
+        >>> input = torch.rand(32, T, 100, 2) #(B, T, N, 2)
+        >>> inputs = [input for _ in range(3)] # iteration
+        >>> target = torch.rand(32, T, 100, 2) #(B, T, N, 2)
+        >>> valid = torch.randint(0, 2, (32, T, 100), dtype=torch.bool)
+        >>> criterion = SequenceLoss()
+        >>> output = criterion(inputs, target, vai)
+        >>> output.backward()
+
+    """
+
+    def __init__(self, gamma: float = 0.8, mean_mode: str = "mean"):
+        super().__init__()
         self._gamma = gamma
-        self._max_flow = max_flow
-        
-    def forward(self, input:list[Tensor], target:Tensor, valids:Tensor)->Tensor:
-        return sequence_loss(input=input, 
-                             target=target, 
-                             valids=valids,
-                             gamma=self._gamma, 
-                             max_flow=self._max_flow)
+        self._mean_mode = mean_mode
+
+    def forward(
+        self, inputs: list[Tensor], target: Tensor, valid: Tensor | None = None
+    ) -> Tensor:
+        return sequence_loss(
+            inputs=inputs,
+            target=target,
+            valid=valid,
+            gamma=self._gamma,
+            mean_mode=self._mean_mode,
+        )

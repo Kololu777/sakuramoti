34568baeb4f4d45cdbb645937f6253096d161753
Kololu777 <kosiynon31kos@gmail.com>
2024-10-10 19:48:50 +0900

    Create Omnimotion Prepare Process

--- demo/omnimotion/preprocessing/filter_raft.py
+++ demo/omnimotion/preprocessing/filter_raft.py
@@ -1,90 +1,100 @@
-"""
-This script filters the raft optical flow using cycle consistency and appearance consistency
-checks (using dino features), and produces the following files:
-
-raft masks: h x w x 3 for each pair of flows, first channel stores the mask for cycle consistency,
-            second channel stores the mask for occlusion (i.e., regions that detected as occluded
-            where the prediction is likely to be reliable using double cycle consistency checks).
-count_maps: h x w for each frame (uint16), contains the number of valid correspondences for each pixel
-            across all frames.
-flow_stats.json: contains the total number of valid correspondences between each pair of frames.
-"""
-
 import os
-import glob
 import json
 import argparse
-import warnings
+from pathlib import Path
 
 import numpy as np
 import torch
 import imageio
 import torch.nn.functional as F
-from tqdm import tqdm
-from chain_raft import gen_grid, normalize_coords
-
-warnings.filterwarnings("ignore")
-
-DEVICE = "cuda"
+from sakuramoti.geometry.grid import generate_grid, normalize_coords
 
 
-def run_filtering(args):
-    feature_name = "dino"
-    scene_dir = args.data_dir
-    print(f"flitering raft optical flow for {scene_dir}....")
+def opj_and_mkdir(dir: str, *args) -> str:
+    out_dir: str = os.path.join(dir, *args)
 
-    img_files = sorted(glob.glob(os.path.join(scene_dir, "color", "*")))
-    num_imgs = len(img_files)
-    pbar = tqdm(total=num_imgs * (num_imgs - 1))
-
-    out_flow_stats_file = os.path.join(scene_dir, "flow_stats.json")
-    out_dir = os.path.join(scene_dir, "raft_masks")
     os.makedirs(out_dir, exist_ok=True)
+    return out_dir
 
-    count_out_dir = os.path.join(scene_dir, "count_maps")
-    os.makedirs(count_out_dir, exist_ok=True)
 
-    h, w = imageio.imread(img_files[0]).shape[:2]
-    grid = gen_grid(h, w, device=DEVICE).permute(2, 0, 1)[None]
-    grid_normed = normalize_coords(grid.squeeze().permute(1, 2, 0), h, w)  # [h, w, 2]
+def get_feature(features: list[torch.Tensor], idx: int, grid_normed: torch.Tensor):
+    feature_i = features[idx].permute(2, 0, 1)[None]  # [1, H_p, W_p, Feat]
+    # feature_i_sampled shape: [H, W, Feat].
+    feature_i_sampled = F.grid_sample(feature_i, grid_normed[None], align_corners=True)[0].permute(1, 2, 0)
+    return feature_i_sampled
+
+
+if __name__ == "__main__":
+    device = "cuda"
+    feature_name = "dino"
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_dir", type=str, default="", help="dataset dir")
+    parser.add_argument("--cycle_th", type=float, default=3.0, help="threshold for cycle consistency error")
+    parser.add_argument("--similarity_th", type=float, default=0.5, help="threshold for cosine similarity")
+    args = parser.parse_args()
+
+    # Get image files
+    img_files = sorted((Path(args.data_dir) / "color").glob("*.png"))
+    num_imgs = len(img_files)
+
+    # Prepare output directories
+    out_flow_stats_file = os.path.join(args.data_dir, "flow_stats.json")
+    out_dir = opj_and_mkdir(args.data_dir, "raft_masks")
+
+    count_out_dir = opj_and_mkdir(args.data_dir, "count_maps")
+
+    # check h and w size.
+    h, w, _ = imageio.v3.imread(img_files[0]).shape
+    grid = generate_grid(h, w, device=device, homogeneous=False).permute(2, 0, 1)[None]
+
+    grid_normed = normalize_coords(grid.squeeze().permute(1, 2, 0), h, w, no_shift=True)  # [h, w, 2]
 
     features = [
-        torch.from_numpy(
-            np.load(os.path.join(scene_dir, "features", feature_name, os.path.basename(img_file) + ".npy"))
-        )
+        torch.from_numpy(np.load(Path(args.data_dir) / "features" / feature_name / (Path(img_file).stem + ".npy")))
         .float()
-        .to(DEVICE)
+        .to(device)
         for img_file in img_files
     ]
-
+    # Confusion Matrix
+    # Exhaustive
     flow_stats = {}
-    count_maps = np.zeros((num_imgs, h, w), dtype=np.uint16)
+    count_maps = np.zeros((num_imgs, h, w), np.uint16)
     for i in range(num_imgs):
         imgname_i = os.path.basename(img_files[i])
-        feature_i = features[i].permute(2, 0, 1)[None]
-        feature_i_sampled = F.grid_sample(feature_i, grid_normed[None], align_corners=True)[0].permute(1, 2, 0)
-
+        feature_i_sampled = get_feature(features, i, grid_normed)
         for j in range(num_imgs):
             if i == j:
                 continue
             frame_interval = abs(i - j)
-            imgname_j = os.path.basename(img_files[j])
-            flow_f = np.load(os.path.join(scene_dir, "raft_exhaustive", f"{imgname_i}_{imgname_j}.npy"))
-            flow_f = torch.from_numpy(flow_f).float().permute(2, 0, 1)[None].cuda()
-            flow_b = np.load(os.path.join(scene_dir, "raft_exhaustive", f"{imgname_j}_{imgname_i}.npy"))
-            flow_b = torch.from_numpy(flow_b).float().permute(2, 0, 1)[None].cuda()
 
-            coord2 = flow_f + grid
-            coord2_normed = normalize_coords(coord2.squeeze().permute(1, 2, 0), h, w)  # [h, w, 2]
-            flow_21_sampled = F.grid_sample(flow_b, coord2_normed[None], align_corners=True)
-            map_i = flow_f + flow_21_sampled
+            imgname_j = os.path.basename(img_files[j])
+
+            # step(1)
+            # Load Estimated Optical Flow by RAFT model.
+            # computed discrepancy i -> j and j -> i.
+            # i ->
+            flow_forward = np.load(Path(args.data_dir) / "raft_exhaustive" / f"{imgname_i}_{imgname_j}.npy")
+            flow_forward = torch.from_numpy(flow_forward).float().permute(2, 0, 1)[None].cuda()
+            coord2 = flow_forward + grid  # absolute coordinate.
+            coord2_normed = normalize_coords(coord2, h, w, no_shift=True)
+            # j -> i
+            flow_backward = np.load(Path(args.data_dir) / "raft_exhaustive" / f"{imgname_j}_{imgname_i}.npy")
+            flow_backward = torch.from_numpy(flow_backward).float().permute(2, 0, 1)[None].cuda()
+            feature_21_sampled = F.grid_sample(flow_backward, coord2_normed[None], align_corners=True)[0].permute(
+                1, 2, 0
+            )
+            # discrepancy i -> j and j -> i
+            map_i = flow_forward + feature_21_sampled
             fb_discrepancy = torch.norm(map_i.squeeze(), dim=0)
             mask_cycle = fb_discrepancy < args.cycle_th
 
-            feature_j = features[j].permute(2, 0, 1)[None]
-            feature_j_sampled = F.grid_sample(feature_j, coord2_normed[None], align_corners=True)[0].permute(1, 2, 0)
-            feature_sim = torch.cosine_similarity(feature_i_sampled, feature_j_sampled, dim=-1)
-            feature_mask = feature_sim > 0.5
+            # step(2)
+            # Load Foundation model Feature.
+            # cosine similarity for i and j.
+            feature_j_sampled = get_feature(features, j, grid_normed)
+            sim_ij = F.cosine_similarity(feature_i_sampled, feature_j_sampled, dim=-1)
+            feature_mask = sim_ij > args.similarity_th
 
             mask_cycle = mask_cycle * feature_mask if frame_interval >= 3 else mask_cycle
 
@@ -94,11 +104,14 @@ def run_filtering(args):
             if frame_interval < 3:
                 coord_21 = grid + map_i  # [1, 2, h, w]
                 coord_21_normed = normalize_coords(coord_21.squeeze().permute(1, 2, 0), h, w)  # [h, w, 2]
-                flow_22 = F.grid_sample(flow_f, coord_21_normed[None], align_corners=True)
-                fbf_discrepancy = torch.norm((coord_21 + flow_22 - flow_f - grid).squeeze(), dim=0)
+
+                flow_22 = F.grid_sample(flow_forward, coord_21_normed[None], align_corners=True)
+                fbf_discrepancy = torch.norm((coord_21 + flow_22 - flow_forward - grid).squeeze(), dim=0)
+
                 mask_in_range = (coord2_normed.min(dim=-1)[0] >= -1) * (coord2_normed.max(dim=-1)[0] <= 1)
                 mask_occluded = (fbf_discrepancy < args.cycle_th) * (fb_discrepancy > args.cycle_th * 1.5)
                 mask_occluded *= mask_in_range
+
             else:
                 mask_occluded = torch.zeros_like(mask_cycle)
 
@@ -109,23 +122,10 @@ def run_filtering(args):
                 flow_stats[imgname_i] = {}
             flow_stats[imgname_i][imgname_j] = np.sum(out_mask).item()
             count_maps[i] += out_mask.sum(axis=-1).astype(np.uint16)
-            pbar.update(1)
 
-    pbar.close()
-    with open(out_flow_stats_file, "w") as fp:
-        json.dump(flow_stats, fp)
+    with open(out_flow_stats_file, "w") as f:
+        json.dump(flow_stats, f)
 
     for i in range(num_imgs):
-        img_name = os.path.basename(img_files[i])
-        imageio.imwrite(os.path.join(count_out_dir, img_name.replace(".jpg", ".png")), count_maps[i])
-
-    print(f"filtering raft optical flow for {scene_dir} is done\n")
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--data_dir", type=str, default="", help="dataset dir")
-    parser.add_argument("--cycle_th", type=float, default=3.0, help="threshold for cycle consistency error")
-    args = parser.parse_args()
-
-    run_filtering(args)
+        save_name = Path(count_out_dir) / img_files[i].with_suffix(".png").name
+        imageio.imwrite(save_name, count_maps[i])
